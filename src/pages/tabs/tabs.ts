7391e211f318c8665b71a85a49cf322d31de1ef2
Rajasuresh <rajasuresh.j@pearldatadirect.com>
2017-07-31 19:22:56 +0530

    Integrated Barcode Scanner App

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,6 +4,8 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -13,7 +15,16 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(private barcodeScanner: BarcodeScanner) {
+    
+  }
 
+  openBarCodeScanner() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log("scanned");
+      // Success! Barcode data is here
+    }, (err) => {
+      // An error occurred
+    });
   }
 }
